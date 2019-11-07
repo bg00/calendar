9cd39d210cc7ed36c1c3f3676ebc3abf1d3adb53
Bailey Garrett <baileyagarrett@gmail.com>
2019-11-06 18:15:27 -0600

    progress on html and color change on hour

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var currentTime = moment().format("dddd, MMMM Do", true);
   var currentDay = document.getElementById("currentDay");
   currentDay.append(currentTime);
-
-  console.log(currentTime);
+    console.log(currentTime);
 });
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+
+$(function() {
+  var today = new Date(),
+    thisTime =
+      today.getHours() + "-" + (today.getMinutes() + 1) + "-" + today.getSeconds();
+  $("#input").each(function() {
+    var specifiedDate = $(this).data("row");
+    if (specifiedDate === thisTime) {
+      $(this).addClass("current");
+    } else if (_thisTime => specifiedDate) {
+      $(this).addClass("past");
+    } else if (_thisTime => specifiedDate) {
+      $(this).addClass("future");
+    }
+  });
+});
+});
+
